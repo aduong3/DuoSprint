9a676986d990c923966d69ced2331d4819749c71
aduong3 <mythalink@gmail.com>
2025-07-04 13:51:55 -0700

    Remove boilerplate given by Vite, add Routes for Home and Sign Up pages.
    I will be listing the components for a MVP in a separate app/file and try to implement software development lifecycle methodologies like agile.

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Home() {
+  return (
+    <div className="py-3 px-2">
+      <Link
+        to="/accounts/signup"
+        className="text-xl bg-gray-200 py-1 px-2 rounded-lg"
+      >
+        Sign Up
+      </Link>
+    </div>
+  );
+}
